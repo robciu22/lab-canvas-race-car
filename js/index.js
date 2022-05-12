c3f20324e92d4cc39acb28900add7ca2cd0c11a8
RoLegs <rolegs@etik.com>
2022-05-12 14:12:00 +0200

    solvedCanvasRaceCar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+let canvas = document.querySelector('canvas')
+let startScreen = document.querySelector('.game-intro')
+let roadScreen = document.querySelector('.road')
 
-  function startGame() {}
+
+window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+        //the next line is for proofing if...
+        console.log("Startbutton was clicked")
+        startGame();
+    };
+
+    function startGame() {
+        startScreen.style.display = 'none'
+            //now only the road-screen has to appear
+        roadScreen.style.display = 'visible'
+    }
 };
